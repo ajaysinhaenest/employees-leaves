8cf8781ebab37737b4403077993d66cb62b56d0b
ajaysinhaenest <ajaysinha.enest@gmail.com>
2023-11-08 11:36:48 +0530

    implemented block user

--- src/Pages/Dashboard/Dashboard.tsx
+++ src/Pages/Dashboard/Dashboard.tsx
@@ -1,56 +1,56 @@
-import { useState, useEffect } from "react"
-import { Box } from "@mui/material"
-import Sidebar from "./Components/Sidebar/Sidebar"
-import MainContainer from "./Components/MainContainer/MainContainer"
-import AddEmployeeForm from "./Components/AddEmployeeForm/AddEmployeeForm"
-import { IEmployee } from "../../Shared/Interfaces/employee.interface"
-import { employeesListData } from "../../Shared/Utils/Constant"
+import { useState, useEffect } from 'react'
+import { Box } from '@mui/material'
+import Sidebar from './Components/Sidebar/Sidebar'
+import MainContainer from './Components/MainContainer/MainContainer'
+import AddEmployeeForm from './Components/AddEmployeeForm/AddEmployeeForm'
+import { IEmployee } from '../../Shared/Interfaces/employee.interface'
+import { employeesListData } from '../../Shared/Utils/Constant'
 
 const Dashboard = () => {
-  const [open, setOpen] = useState(false)
-  const [employeesList, setEmployeesList] = useState<IEmployee[]>([])
-  const [filteredList, setFilteredList] = useState<IEmployee[]>([])
+    const [open, setOpen] = useState(false)
+    const [employeesList, setEmployeesList] = useState<IEmployee[]>([])
+    const [filteredList, setFilteredList] = useState<IEmployee[]>([])
 
-  useEffect(() => {
-    // localStorage.setItem("employeesList", JSON.stringify(employeesListData))
+    useEffect(() => {
+        // localStorage.setItem('employeesList', JSON.stringify(employeesListData))
 
-    // const updateData = employeesListData.map((data) => {
-    //   return {
-    //     name: data.firstName + " " + data.lastName,
-    //     email: data.email,
-    //     password: data.password,
-    //     admin: false,
-    //     block: false,
-    //     blockCount: 0,
-    //   }
-    // })
-    // console.log(updateData)
-    // localStorage.setItem("users", JSON.stringify(updateData))
+        // const updateData = employeesListData.map((data) => {
+        //     return {
+        //         name: data.firstName + ' ' + data.lastName,
+        //         email: data.email,
+        //         password: data.password,
+        //         admin: false,
+        //         block: false,
+        //         blockCount: 0,
+        //     }
+        // })
+        // console.log(updateData)
+        // localStorage.setItem('users', JSON.stringify(updateData))
 
-    const employeesListData = JSON.parse(
-      localStorage.getItem("employeesList") || "null"
+        const employeesListData = JSON.parse(
+            localStorage.getItem('employeesList') || 'null',
+        )
+        setEmployeesList(employeesListData)
+        setFilteredList(employeesListData)
+    }, [])
+
+    return (
+        <Box display='flex'>
+            <Sidebar open={open} setOpen={setOpen} />
+            <MainContainer
+                filteredList={filteredList}
+                setFilteredList={setFilteredList}
+                employeesList={employeesList}
+                setEmployeesList={setEmployeesList}
+            />
+            <AddEmployeeForm
+                open={open}
+                setOpen={setOpen}
+                employeesList={employeesList}
+                setEmployeesList={setEmployeesList}
+            />
+        </Box>
     )
-    setEmployeesList(employeesListData)
-    setFilteredList(employeesListData)
-  }, [])
-
-  return (
-    <Box display="flex">
-      <Sidebar open={open} setOpen={setOpen} />
-      <MainContainer
-        filteredList={filteredList}
-        setFilteredList={setFilteredList}
-        employeesList={employeesList}
-        setEmployeesList={setEmployeesList}
-      />
-      <AddEmployeeForm
-        open={open}
-        setOpen={setOpen}
-        employeesList={employeesList}
-        setEmployeesList={setEmployeesList}
-      />
-    </Box>
-  )
 }
 
 export default Dashboard
--- src/Pages/Login/Components/LoginForm.tsx
+++ src/Pages/Login/Components/LoginForm.tsx
@@ -1,127 +1,144 @@
-import { TextField, Typography, Box, Button } from "@mui/material"
-import getMobxReactFormValidation from "../../../Shared/MobxValidation/MobxReactFormValidation"
-import { useMemo, useState } from "react"
-import { inject, observer } from "mobx-react"
-import { IUser } from "../../../Shared/Interfaces/registration.interface"
-import { Link, useNavigate } from "react-router-dom"
-import { loginFields } from "../Fields/login.fields"
-import { toast } from "react-toastify"
+import { TextField, Typography, Box, Button } from '@mui/material'
+import getMobxReactFormValidation from '../../../Shared/MobxValidation/MobxReactFormValidation'
+import { useEffect, useMemo, useState } from 'react'
+import { inject, observer } from 'mobx-react'
+import { IUser } from '../../../Shared/Interfaces/registration.interface'
+import { Link, useNavigate } from 'react-router-dom'
+import { loginFields } from '../Fields/login.fields'
+import { toast } from 'react-toastify'
 
 const LoginForm = observer(() => {
-  const [users, setUsers] = useState<IUser[]>([])
-  const navigate = useNavigate()
-  const form = useMemo(() => getMobxReactFormValidation(loginFields), [])
+    const [users, setUsers] = useState<IUser[]>([])
+    const navigate = useNavigate()
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
+    const form = useMemo(() => getMobxReactFormValidation(loginFields), [])
 
-    const { email, password } = form.values()
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
 
-    const storedUsers = JSON.parse(localStorage.getItem("users") || "null")
-    setUsers(storedUsers)
-    const login_user: IUser[] = storedUsers?.filter((el: IUser, i: number) => {
-      return el.email === email || el.password === password
-    })
-    console.log(login_user)
+        const { email, password } = form.values()
 
-    if (login_user?.length) {
-      if (
-        login_user[0]?.email === email &&
-        login_user[0]?.password === password
-      ) {
-        localStorage.setItem("login_user", JSON.stringify(login_user))
+        const storedUsers = JSON.parse(localStorage.getItem('users') || 'null')
 
-        if (login_user[0].admin) {
-          navigate("/dashboard")
-        } else {
-          navigate("/employee")
+        const login_user: IUser[] = storedUsers?.filter(
+            (el: IUser, i: number) => {
+                return el.email === email || el.password === password
+            },
+        )
+        console.log(login_user)
+
+        if (login_user?.length) {
+            if (login_user[0].block === false) {
+                if (
+                    login_user[0]?.email === email &&
+                    login_user[0]?.password === password
+                ) {
+                    const user: IUser[] = storedUsers.map(
+                        (el: IUser, i: number) => {
+                            if (el === login_user[0]) {
+                                return {
+                                    ...el,
+                                    block: false,
+                                    blockCount: 0,
+                                }
+                            }
+                            return el
+                        },
+                    )
+                    // setUsers(user)
+                    localStorage.setItem('users', JSON.stringify(user))
+
+                    localStorage.setItem(
+                        'login_user',
+                        JSON.stringify(login_user),
+                    )
+
+                    if (login_user[0].admin) {
+                        navigate('/dashboard')
+                    } else {
+                        navigate('/employee')
+                    }
+                    form.clear()
+                    return null
+                } else {
+                    if (login_user[0].blockCount < 2) {
+                        toast.error('Login failed. Check your credentials.')
+                    } else {
+                        toast.error('Login failed. You are blocked..')
+                    }
+
+                    const user: IUser[] = storedUsers.map(
+                        (el: IUser, i: number) => {
+                            if (el === login_user[0]) {
+                                if (el.blockCount === 2) {
+                                    return {
+                                        ...el,
+                                        blockCount: 0,
+                                        block: true,
+                                    }
+                                }
+                                if (el.blockCount < 2 && el.block === false)
+                                    return {
+                                        ...el,
+                                        blockCount: el.blockCount + 1,
+                                    }
+                            }
+                            return el
+                        },
+                    )
+                    // setUsers(user)
+                    localStorage.setItem('users', JSON.stringify(user))
+                    return null
+                }
+            }
         }
-        form.clear()
-        return null
-      } else {
-        toast.error("Login failed. Check your credentials.")
-        let user: IUser
-        if (login_user[0].blockCount === 3) {
-          user = {
-            ...login_user[0],
-            block: true,
-            blockCount: 0,
-          }
-          user = {
-            ...login_user[0],
-            block: false,
-            blockCount: login_user[0].blockCount + 1,
-          }
-          setUsers((pre) => [...pre, { ...user }])
-          console.log(user)
-        }
-        // const user: IUser[] = storedUsers.map((el: IUser, i: number) => {
-        //   if (el === login_user[0]) {
-        //     if (el.blockCount === 3) {
-        //       return {
-        //         ...el,
-        //         blockCount: 0,
-        //         block: true,
-        //       }
-        //     }
-        //     return {
-        //       ...el,
-        //       blockCount: ++el.blockCount,
-        //     }
-        //   }
-        //   return el
-        // })
-
-        return null
-      }
+        toast.error('user does not exist')
     }
-    toast.error("user does not exist")
-  }
-  //   console.log(users)
-  return (
-    <Box>
-      <form action="">
-        <Box display="flex" mb={2}>
-          <Typography variant="h6" mr={2} color="initial">
-            Email:
-          </Typography>
-          <TextField
-            size="small"
-            fullWidth
-            {...form.$("email").bind()}
-            variant="outlined"
-          />
+    console.log(users)
+    return (
+        <Box>
+            <form action=''>
+                <Box display='flex' mb={2}>
+                    <Typography variant='h6' mr={2} color='initial'>
+                        Email:
+                    </Typography>
+                    <TextField
+                        size='small'
+                        fullWidth
+                        {...form.$('email').bind()}
+                        variant='outlined'
+                    />
+                </Box>
+                <Box display='flex' mb={2}>
+                    <Typography variant='h6' mr={2} color='initial'>
+                        Password:
+                    </Typography>
+                    <TextField
+                        size='small'
+                        fullWidth
+                        variant='outlined'
+                        {...form.$('password').bind()}
+                    />
+                </Box>
+                <Typography my={1}>
+                    Create Your Account ?
+                    <Link to='/' color='error'>
+                        <Button color='error'> Register</Button>
+                    </Link>
+                </Typography>
+                <Button
+                    type='submit'
+                    size='small'
+                    fullWidth
+                    variant='contained'
+                    color='secondary'
+                    onClick={(e) => handleSubmit(e)}
+                >
+                    Login
+                </Button>
+            </form>
         </Box>
-        <Box display="flex" mb={2}>
-          <Typography variant="h6" mr={2} color="initial">
-            Password:
-          </Typography>
-          <TextField
-            size="small"
-            fullWidth
-            variant="outlined"
-            {...form.$("password").bind()}
-          />
-        </Box>
-        <Typography my={1}>
-          Create Your Account ?
-          <Link to="/" color="error">
-            <Button color="error"> Register</Button>
-          </Link>
-        </Typography>
-        <Button
-          type="submit"
-          size="small"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          onClick={(e) => handleSubmit(e)}
-        >
-          Login
-        </Button>
-      </form>
-    </Box>
-  )
+    )
 })
 
-export default inject("employeeStore")(LoginForm)
+export default inject('employeeStore')(LoginForm)
